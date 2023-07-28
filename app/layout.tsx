e6989c6c77a4a2669ae66bc433444cb6d4f685ff
Nikita Stogniy <ggsailgg@gmail.com>
2023-07-28 12:04:18 +0300

    added YM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,33 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="ru">
+			<head>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+				ym(94456909, "init", {
+					clickmap:true,
+					trackLinks:true,
+					accurateTrackBounce:true,
+					webvisor:true
+			   });
+			
+              `,
+					}}
+				/>
+			</head>
 			<body className={inter.className}>{children}</body>
+			<div>
+				<img
+					src="https://mc.yandex.ru/watch/94456909"
+					style={{ position: "absolute", left: "-9999px" }}
+					alt=""
+				/>
+			</div>
 		</html>
 	);
 }
